1aca867bdaa4ae66364444e104cfb1f0a1504f1e
Luis Fernando <86975316+LuiFeAn@users.noreply.github.com>
2021-10-14 11:38:18 -0300

    1.0.0
    Beta

---
+++ app/programs.js
@@ -0,0 +1,9 @@
+let programs = [
+    {name: "Google Chrome", icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/240px-Google_Chrome_icon_%28September_2014%29.svg.png"},
+    {name: "Lixeira",icon:"https://pt.teles-relay.com/wp-content/uploads/2020/01/Recycle-Bin.png"},
+    {name: "Gerenciador",icon:"https://www.wikihow.com/images_en/f/fe/Windowswindows7_explorer.png"},
+    {name: "Visual Studio Code", icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png"}
+]
+let folders = [
+    {name:"",icon:"https://www.iconpacks.net/icons/2/free-folder-icon-1484-thumb.png"}
+]
